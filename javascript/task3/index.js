a2a94a145d2db63a1a4a32d963ea4130635367ea
creamstew <umetsu@pin-japan.com>
2021-03-23 22:59:49 +0900

    デフォルト引数を元に戻す

--- javascript/task3/index.js
+++ javascript/task3/index.js
@@ -1,13 +1,13 @@
 const tasks = [];
 
-function showTask(statusFilter = []) {
+function showTask(statusFilter = false) {
   const tableBody = document.getElementById('task_body');
 
   while (tableBody.firstChild) {
     tableBody.removeChild(tableBody.firstChild);
   }
 
-  const visibleTasks = statusFilter.length ? statusFilter : tasks;
+  const visibleTasks = statusFilter || tasks;
   visibleTasks.forEach((task) => {
     const tableRecord = document.createElement('tr');
     tableBody.appendChild(tableRecord);
